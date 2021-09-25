f7c5c33a2204d1d5ebcba4e114fe0ec4bd7596a7
Jeffrey Gu <jgu3661@gmail.com>
2021-09-25 11:15:25 -0400

    tyrant alg

--- todoketetyrant.py
+++ todoketetyrant.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python
 
-# This is a dummy peer that just illustrates the available information your peers 
-# have available.
-
-# You'll want to copy this file to AgentNameXXX.py for various versions of XXX,
-# probably get rid of the silly logging messages, and then add more logic.
-
 import random
 import logging
 
 from messages import Upload, Request
 from util import even_split
 from peer import Peer
+from collections import defaultdict
 
 class TodoketeTyrant(Peer):
+    
     def post_init(self):
         print(("post_init(): %s here!" % self.id))
-        self.dummy_state = dict()
-        self.dummy_state["cake"] = "lie"
+        self.alpha = 0.20
+        self.r = 3
+        self.gamma = 0.10
+        self.dinit = 3
+        self.uinit = 3
     
     def requests(self, peers, history):
         """
@@ -32,35 +31,34 @@ class TodoketeTyrant(Peer):
         needed_pieces = list(filter(needed, list(range(len(self.pieces)))))
         np_set = set(needed_pieces)  # sets support fast intersection ops.
 
-
-        logging.debug("%s here: still need pieces %s" % (
-            self.id, needed_pieces))
-
-        logging.debug("%s still here. Here are some peers:" % self.id)
-        for p in peers:
-            logging.debug("id: %s, available pieces: %s" % (p.id, p.available_pieces))
-
-        logging.debug("And look, I have my entire history available too:")
-        logging.debug("look at the AgentHistory class in history.py for details")
-        logging.debug(str(history))
-
         requests = []   # We'll put all the things we want here
-        # Symmetry breaking is good...
-        random.shuffle(needed_pieces)
         
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
-        peers.sort(key=lambda p: p.id)
+        # Sort peers randomly
+        random.shuffle(peers)
+
+        # Determine rarity of each piece (e.g. how many peers own each piece)
+        rarity = defaultdict(int)
+        for peer in peers:
+            for piece_id in peer.available_pieces:
+                rarity[piece_id] += 1
+
         # request all available pieces from all peers!
         # (up to self.max_requests from each)
+        random.shuffle(peers)
         for peer in peers:
             av_set = set(peer.available_pieces)
             isect = av_set.intersection(np_set)
+            isect = list(isect)
+            
             n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
+            
+            # Ask for n rarest pieces, rarest first
+            if len(isect) > 0:
+                # Break symmetry
+                random.shuffle(isect)
+                isect.sort(key = lambda x: rarity[x])
+                isect = isect[:n]
+            for piece_id in isect:
                 # aha! The peer has this piece! Request it.
                 # which part of the piece do we need next?
                 # (must get the next-needed blocks in order)
@@ -94,17 +92,54 @@ class TodoketeTyrant(Peer):
             chosen = []
             bws = []
         else:
-            logging.debug("Still here: uploading to a random peer")
-            # change my internal state for no reason
-            self.dummy_state["cake"] = "pie"
+            
+            # List of peers who have made requests
+            requesting_peers = [request.requester_id for request in requests]
 
-            request = random.choice(requests)
-            chosen = [request.requester_id]
-            # Evenly "split" my upload bandwidth among the one chosen requester
-            bws = even_split(self.up_bw, len(chosen))
+            # Create download rate estimates for peers
+            dlr_ests = defaultdict(int)
+            for peer in peers:
+                dlr_ests[peer] = self.dinit
+                for round_dls in reversed(history.downloads[peer]):
+                    if round_dls:
+                        dlr_ests[peer] = len(round_dls)
+                        break
+                    
+            # Create upload rate estimates for peers
+            ulr_ests = defaultdict(int)
+            for peer in requesting_peers:
+                ulr = self.uinit
+                for idx, round_uls in enumerate(history.uploads[peer]):
+                    # If we unchoked peer, update
+                    if round_uls: 
+                        # If peer didn't unchoke us, increase ulr
+                        if not history.downloads[peer][idx]:
+                            ulr = (1 + self.alpha) * ulr
+                        # If peer unchoked us for last r rounds, decrease ulr
+                        elif round >= self.r:
+                            last_r = True
+                            for prev_round in reversed(history.downloads[peer][idx-self.r+1:idx]):
+                                if not prev_round:
+                                    last_r = False
+                            if last_r:
+                                ulr = (1 - self.gamma) * ulr
+                ulr_ests[peer] = ulr
 
-        # create actual uploads out of the list of peer ids and bandwidths
+            # Sort requesting peers by decreasing dlr/ulr
+            requesting_peers = sorted(requesting_peers, key=lambda peer: dlr_ests[peer]/ulr_ests[peer], reverse=True)
+            
+            # Add upload slots until cap is reached
+            chosen = []
+            bws = []
+            bw_remaining = self.up_bw
+            for peer in requesting_peers:
+                bw_remaining -= ulr_ests[peer]
+                if bw_remaining < 0:
+                    break
+                chosen.append(peer)
+                bws.append(ulr_ests[peer])
+
+        # Create actual uploads out of the list of peer ids and bandwidths
         uploads = [Upload(self.id, peer_id, bw)
                    for (peer_id, bw) in zip(chosen, bws)]
-            
         return uploads
